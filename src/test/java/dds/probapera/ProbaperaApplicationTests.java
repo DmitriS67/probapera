c3676d8ff3e3b567358bfcb0262474e14622ca01
dmitri <dmitri.saveliev@netum.fi>
2020-11-04 10:48:34 +0200

    commit 1

---
+++ src/test/java/dds/probapera/ProbaperaApplicationTests.java
@@ -0,0 +1,13 @@
+package dds.probapera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbaperaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
