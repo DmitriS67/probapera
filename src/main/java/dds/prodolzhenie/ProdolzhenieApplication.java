d4acfa635a3e5a237990822836b7791529729a84
dmitri <dmitri.saveliev@netum.fi>
2020-11-05 13:03:40 +0200

    master commit

---
+++ src/main/java/dds/prodolzhenie/ProdolzhenieApplication.java
@@ -0,0 +1,14 @@
+package dds.probapera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdolzhenieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProbaperaApplication.class, args);
+    }
+
+}
+
